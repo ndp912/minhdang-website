ad252fc4b4ddb59bd80295335c6d312aac405aec
ndp912 <phucpascal@gmail.com>
2024-09-14 14:08:56 +0700

    Add files via upload

--- index/scripts.js
+++ index/scripts.js
@@ -133,28 +133,5 @@ gsap.to(".pre-loader", 2, {
   delay: 4,
 });
 
-let elements = document.querySelectorAll(".text");
 
-      elements.forEach((element) => {
-        let innerText = element.innerText;
-        element.innerHTML = "";
 
-        let textContainer = document.createElement("div");
-        textContainer.classList.add("block");
-
-        for (let letter of innerText) {
-          let span = document.createElement("span");
-          span.innerText = letter.trim() === "" ? "\xa0" : letter;
-          span.classList.add("letter");
-          textContainer.appendChild(span);
-        }
-
-        element.appendChild(textContainer);
-        element.appendChild(textContainer.cloneNode(true));
-      });
-
-      elements.forEach((element) => {
-        element.addEventListener("mouseover", () => {
-          element.classList.remove("play");
-        });
-      });
